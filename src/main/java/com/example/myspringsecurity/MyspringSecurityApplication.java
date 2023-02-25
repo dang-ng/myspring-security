88de9b584ee89fbce21cf254bc32ca2c48672e6f
dang-ng <nguyen.hai.dang@icloud.com>
2023-02-25 14:30:54 +0700

    first commit

---
+++ src/main/java/com/example/myspringsecurity/MyspringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myspringsecurity/MyspringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
